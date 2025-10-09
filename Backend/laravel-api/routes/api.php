34ad0098d3a9cfb686f053cb648cf4173c5ae642
starden3 <denys2004droga@gmail.com>
2025-10-09 22:35:06 +0100

    Delete/Create/Update de Clientes

--- Backend/laravel-api/routes/api.php
+++ Backend/laravel-api/routes/api.php
@@ -9,3 +9,8 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/clients', [\App\Http\Controllers\ClientController::class, 'index']);
 Route::get('/clients/{client}', [\App\Http\Controllers\ClientController::class, 'show']);
+//Route::get('/clients', [\App\Http\Controllers\ClientController::class, 'create ']);
+Route::post('/clients', [\App\Http\Controllers\ClientController::class, 'store']);
+Route::put('/clients/{client}', [\App\Http\Controllers\ClientController::class, 'update']);
+Route::delete('/clients/{client}', [\App\Http\Controllers\ClientController::class, 'destroy']);
+
