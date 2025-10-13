126554199f6b3d231a5b6ddb984fa3ddb87d17af
ThroughTheUnknown <backsomehow@gmail.com>
2025-10-13 22:00:55 +0100

    Created Users, Profiles and Roles

--- Backend/laravel-api/database/seeders/ProfileSeeder.php
+++ Backend/laravel-api/database/seeders/ProfileSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
-class UserSeeder extends Seeder
+class ProfileSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::factory(30)->create();
+
+
     }
 }
