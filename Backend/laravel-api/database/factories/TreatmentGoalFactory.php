b93dab69a098ea8ac12d2f5f9fb8cee15d58c5d6
Warsaw <00untrue@gmail.com>
2025-10-19 22:27:07 +0100

    Fixed some more tables.

--- Backend/laravel-api/database/factories/TreatmentGoalFactory.php
+++ Backend/laravel-api/database/factories/TreatmentGoalFactory.php
@@ -6,7 +6,7 @@ use App\Models\Patient;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Treatment_Goal>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TreatmentGoal>
  */
 class TreatmentGoalFactory extends Factory
 {
