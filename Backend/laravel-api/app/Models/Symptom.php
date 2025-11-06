deaeb61c16e80d0931450392406d41b00dd1602e
Warsaw <00untrue@gmail.com>
2025-11-06 21:41:15 +0000

    Created relationships in the Diagnostic and Symptom models

---
+++ Backend/laravel-api/app/Models/Symptom.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Symptom extends Model
+{
+    /** @use HasFactory<\Database\Factories\SymptomFactory> */
+    use HasFactory;
+
+    public function diagnostics()
+    {
+        return $this->belongsToMany(Diagnostic::class, 'diagnostic_symptom');
+    }
+}
