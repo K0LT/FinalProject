f2bc22ac6d08fd01cc066f7b6f687b75bd2006c1
Warsaw <00untrue@gmail.com>
2025-11-07 21:57:12 +0000

    Updated Allergy Controller with Try Catch

---
+++ Backend/laravel-api/app/Http/Controllers/AllergyController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Allergy;
+use App\Http\Requests\StoreAllergyRequest;
+use App\Http\Requests\UpdateAllergyRequest;
+
+class AllergyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+
+            $allergies = Allergy::all();
+            return response()->json([
+                'success' => true,
+                'data' => $allergies,
+                ], 200);
+
+        }catch(\Exception $e){
+
+            \Log::error('Ocorreu um erro ao obter alergias.' . $e->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro ao obter alergias.',
+            ], 500);
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAllergyRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $allergy = Allergy::create($data);
+            return Response()->json([
+                'success' => true,
+                'data' => $allergy
+            ], 200);
+        }
+            catch (\Exception $e) {
+            \Log::error('Ocorreu um erro a criar alergia' . $e->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro a criar alergia:'
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Allergy $allergy)
+    {
+        try {
+            return response()->json([
+                'success' => true,
+                'data' => $allergy
+                ]);
+        }catch(\Exception $e){
+            \Log::error('Ocorreu um erro a mostrar uma alergia' . $e->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro a mostrar uma alergia',
+            ], 500);
+        }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Allergy $allergy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAllergyRequest $request, Allergy $allergy)
+    {
+
+        try {
+            $data = $request->validated();
+            $allergy -> update($data);
+            return response()->json([
+                'success' => true,
+                'data' => $allergy
+            ]);
+        }catch(\Exception $e){
+            \Log::error('Ocorreu um erro a editar uma alergia' . $e->getMessage());
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocorreu um erro a editar uma alergia.'
+            ], 500);
+        }
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Allergy $allergy)
+    {
+        //
+    }
+}
